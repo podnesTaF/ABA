56c88852d6e40eb6b79099b758412e7c090ac7d4
podnesTaF <podnestrade@gmail.com>
2024-11-04 09:05:58 +0000

    update hero

--- aba-frontend/src/components/home/HomeHero.tsx
+++ aba-frontend/src/components/home/HomeHero.tsx
@@ -13,10 +13,10 @@ const HomeHero = ({content}: {content: HomeHeroContent}) => {
 			<ParallaxImage src={getImageUrl(content.backgroundImage.url)} alt={content.backgroundImage.name} width={1800} height={1200} className={'absolute left-0 top-0 w-full h-[110%] object-cover'}  />
 			<div className={"h-[700px] 2xl:h-[680px] mx-auto py-24 px-5 flex items-center flex-col heroElem z-[1] bg-[url('/vectors/transparent-track.svg')] bg-no-repeat bg-center bg-cover md:bg-contain"}>
 				<div className={'flex-1 flex flex-col gap-5 xl:gap-8 items-center justify-center max-w-5xl mx-0 lg:mx-6 xl:mx-12 '}>
-					<Image src={getImageUrl(content.logo.url)} alt={'track'} width={240} height={100} className={'object-cover object-center h-auto'} />
-					{content.title && <h1 className={'text-5xl xl:text-7xl font-extrabold text-center uppercase text-white gradient-title'}>
-						{content.title}
-					</h1>}
+					<Image src={getImageUrl(content.logo.url)} alt={'track'} width={480} height={240} className={'object-cover object-center h-auto'} />
+					{/*{content.title && <h1 className={'text-5xl xl:text-7xl font-extrabold text-center uppercase text-white gradient-title'}>*/}
+					{/*	{content.title}*/}
+					{/*</h1>}*/}
 					<ParseContent text={content.description}  secondaryTitleClassName={'text-center max-w-3xl text-base xl:text-lg font-medium text-gray-100'}/>
 				</div>
 				<div>
